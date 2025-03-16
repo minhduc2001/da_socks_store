3de690b33b74aed63c3ca1d19d245864da2a016d
ducnm <ngoduc2468@gmail.com>
2025-03-16 16:05:06 +0700

    update code

--- backend/src/cart/cart.module.ts
+++ backend/src/cart/cart.module.ts
@@ -6,9 +6,10 @@ import { Cart } from './entities/cart.entity';
 import { CartItem } from './entities/cart-item.entity';
 import { ProductVariant } from '@/product/entities/product-variant.entity';
 import { Bill } from '@/bill/entities/bill.entity';
+import { Product } from '@/product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart, CartItem, ProductVariant, Bill])],
+  imports: [TypeOrmModule.forFeature([Cart, CartItem, ProductVariant, Bill, Product])],
   controllers: [CartController],
   providers: [CartService],
 })
